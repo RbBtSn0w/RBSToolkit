ebb89c283f4657949253f47e94d0b726219582c1
rbbtsn0w <hamiltonsnow@gmail.com>
2022-04-19 12:20:17 +0800

    <chore>(<build>): <pods file include of git env>
    # - type: feat, fix, docs, style, refactor, test, chore
    # - scope: can be empty (eg. if the change is a global or difficult to assign to a single component)
    # - subject: start with verb (such as 'change'), 50-character line
    #
    # body: 72-character wrapped. This should answer:
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    MTL not support official CocoaPods
    # footer:
    # - Include a link to the ticket, if any.
    # - BREAKING CHANGE
    #

---
+++ Example/Pods/Target Support Files/Pods-RBSToolkit_Tests/Pods-RBSToolkit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RBSToolkit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_RBSToolkit_Tests
+@end
---
+++ Example/Pods/Target Support Files/RBSToolkit/RBSToolkit-umbrella.h
@@ -0,0 +1,23 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NSBundle+RBSTool.h"
+#import "NSNumber+RBSNumber.h"
+#import "RBSCGTool.h"
+#import "RBSLargeActionButton.h"
+#import "RBSWeakProxy.h"
+#import "UIImageView+RBSTool.h"
+#import "UILabel+RBSTool.h"
+
+FOUNDATION_EXPORT double RBSToolkitVersionNumber;
+FOUNDATION_EXPORT const unsigned char RBSToolkitVersionString[];
+
